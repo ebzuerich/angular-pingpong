6ac23ef198d488513bc3565addff2eeaa13be173
openscript <1105080+openscript@users.noreply.github.com>
2018-06-16 20:57:29 +0200

    Enhance pinging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { PingService } from './services/ping.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  public constructor(public pingService: PingService) { }
-
-  public ngOnInit(): void {
-    console.log(this.pingService.ping());
-  }
+export class AppComponent {
+  public constructor() { }
 }
--- src/app/services/ping.service.ts
+++ src/app/services/ping.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '../domain/message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PingService {
 
-  constructor() { }
+  constructor(private client: HttpClient) { }
 
   public ping() {
-    return 'pong';
+    return this.client.get<Message>('https://ebzuerich-simple-api.herokuapp.com/delayedping');
   }
 }
